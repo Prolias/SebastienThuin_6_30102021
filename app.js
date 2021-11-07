3462bcf936c354b2f6eb51fc2667a703993293ae
Sébastien Thuin <seb.thuin@gmail.com>
2021-11-07 20:52:37 +0100

    Ajout système de route + controller + model

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
+const authRoutes = require('./routes/auth');
+const sauceRoutes = require('./routes/sauce');
+
 mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.nr0d3.mongodb.net/hot-takes?retryWrites=true&w=majority`)
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(err => console.error(err));
 
-app.get('/api/', (req, res) => {
-    res.send('Accessed to /api/')
-})
+app.use('/api/auth', authRoutes);
+app.use('/api/sauce', sauceRoutes);
 
 module.exports = app;
---
+++ controllers/sauce.js
@@ -0,0 +1,23 @@
+exports.getAllSauces = (req, res) => {
+
+}
+
+exports.getOneSauce = (req, res) => {
+
+}
+
+exports.createSauce = (req, res) => {
+
+}
+
+exports.modifySauce = (req, res) => {
+
+}
+
+exports.deleteSauce = (req, res) => {
+
+}
+
+exports.likeSauce = (req, res) => {
+    
+}
