0a02332062a99d1945e14f756a9dda9eaf7cf2ef
Sébastien Thuin <seb.thuin@gmail.com>
2021-11-07 23:00:08 +0100

    + Ajout logique get sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,13 +1,19 @@
-exports.getAllSauces = (req, res) => {
+const Sauce = require('../models/Sauce');
 
+exports.getAllSauces = (req, res) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
 }
 
 exports.getOneSauce = (req, res) => {
-
+    Sauce.findById({_id: req.params.id})
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
 }
 
 exports.createSauce = (req, res) => {
-
+    
 }
 
 exports.modifySauce = (req, res) => {
