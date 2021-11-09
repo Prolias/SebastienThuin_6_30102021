6e861965c215fc332c870671c9bd4681a76c109e
Sébastien Thuin <seb.thuin@gmail.com>
2021-11-09 16:37:46 +0100

    + Ajout modifySauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -19,12 +19,14 @@ exports.createSauce = (req, res) => {
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
-        .then(() => res.status(201).json({ 'message': 'Sauce created!'}))
+        .then(() => res.status(201).json({ 'message': 'Sauce created!' }))
         .catch(error => res.status(400).json({ error }))
 }
 
 exports.modifySauce = (req, res) => {
-
+    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id})
+        .then(() => res.status(200).json({ 'message': 'Sauce modified!' }))
+        .catch(error => res.status(400).json({ error }))
 }
 
 exports.deleteSauce = (req, res) => {
